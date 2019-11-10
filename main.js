7b56b4f18ee88e57c8c1bcde338caf43e1c40fc6
DinelliFernanda <45576842+DinelliFernanda@users.noreply.github.com>
2019-11-10 13:52:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var vet, busca, str = [];
+
+function cadeia(){
+    
+    for (vet = 1; vet <= 20; vet++){
+        str = prompt("Informe o texto desejado:" );
+        str.length;  
+        busca = prompt("Digite o caractere a ser procurado: ") 
+        str.indexOf(busca);
+
+        if (str.indexOf(busca) == false){
+            alert("O caractere não pertence à cadeia informada!");
+        }
+        else{
+
+            if (vet == '') or (vet > 20);{
+                alert("Atenção!!! A cadeia de caracteres deve ter entre 1 a 20 caracteres!");
+            }
+
+            if(busca == ''){
+                alert("Informe um caractere para ser procurado!")
+            }
+        }
+        
+    }
+
+
+
+}
